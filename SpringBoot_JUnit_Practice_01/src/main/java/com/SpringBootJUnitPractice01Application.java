c0235653f2f77afa3644f8d30b0e05573c4b46d2
Eugen Fazekas <skybolt83@gmail.com>
2021-08-20 15:50:37 +0300

    Practice_01: added some assertions and assumptions tests

---
+++ SpringBoot_JUnit_Practice_01/src/main/java/com/SpringBootJUnitPractice01Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJUnitPractice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJUnitPractice01Application.class, args);
+	}
+
+}
