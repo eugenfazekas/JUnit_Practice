6bc68ca23126cd379911418b3ce6bdc3838896a1
Eugen Fazekas <skybolt83@gmail.com>
2021-08-21 17:16:46 +0300

    SpringBoot_JUnit_Practice_03_Stubs_WebClient

---
+++ SpringBoot_JUnit_Practice_03_Stubs_WebClient/src/main/java/com/SpringBootJUnitPractice03StubsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJUnitPractice03StubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJUnitPractice03StubsApplication.class, args);
+	}
+
+}
