6327acb75519dd512129433f193b0f0469cf355e
Eugen Fazekas <skybolt83@gmail.com>
2021-08-25 15:54:28 +0300

    added SpringBoot_JUnit_Practice_08_Beans with present features

---
+++ SpringBoot_JUnit_Practice_08_Beans_2/src/main/java/com/SpringBootJUnitPractice08Beans2Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJUnitPractice08Beans2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJUnitPractice08Beans2Application.class, args);
+	}
+
+}
